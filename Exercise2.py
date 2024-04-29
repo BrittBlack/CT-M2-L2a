52b9a6199c4bde2ecdf3e338a72d48caaa3edbed
BrittBlack <brittneysblackmon@gmail.com>
2024-04-28 20:23:44 -0500

    updated exercise

--- Exercise2.py
+++ Exercise2.py
@@ -15,8 +15,7 @@ if num2 > num1:
     large = num2
 elif num3 > num1:
     large = num3
-else: 
-    print("The largest number is:", large )
+print("The largest number is:", large )
 
 #
 #
@@ -30,8 +29,7 @@ if num2 < num3:
     small = num1
 elif num3 < num1:
     small = num3
-else:
-    print("The smallest number is:", small)
+print("The smallest number is:", small)
 
 #
 #
@@ -42,10 +40,8 @@ else:
 if num1 == num2 == num3:
     print("All numbers are equal.")
 elif num1 == num2:
-    print(f"{num1} is equal to {num2}." "The largest number is:", large, "The smallest number is:", small)
+    print(f"{num1} is equal to {num2}." "The largest number is:" large, "The smallest number is:" small)
 elif num2 == num3:
-    print(f"{num2} is equal to {num3}.""The largest number is:", large, "The smallest number is:", small)
+    print(f"{num2} is equal to {num3}.""The largest number is:"  large, "The smallest number is:" small)
 elif num1 == num3:
-    print(f"{num1} is equal to {num3}.""The largest number is:", large, "The smallest number is:", small)
-else:
-    print("All numbers are not equal.")
+    print(f"{num1} is equal to {num3}.""The largest number is:" large, "The smallest number is:" small)
